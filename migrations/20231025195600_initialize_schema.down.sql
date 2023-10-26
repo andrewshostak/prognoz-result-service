ab703264132463d8242849c77c921495c1eb9a73
Andriy Shostak <andrew.shostak11@gmail.com>
2023-10-26 12:42:51 +0300

    Add up/down migrations for inital schema

---
+++ migrations/20231025195600_initialize_schema.down.sql
@@ -0,0 +1,10 @@
+begin;
+
+drop table if exists football_api_fixtures;
+drop table if exists subscriptions;
+drop table if exists aliases;
+drop table if exists matches;
+drop table if exists football_api_teams;
+drop table if exists teams;
+
+commit;
---
+++ migrations/20231025195600_initialize_schema.up.sql
@@ -0,0 +1,48 @@
+begin;
+
+create table if not exists teams (
+    id bigserial primary key
+);
+
+create table if not exists football_api_teams (
+    id bigserial primary key,
+    team_id bigserial unique,
+    foreign key (team_id) references teams (id) on update cascade on delete restrict
+);
+
+create table if not exists matches (
+    id bigserial primary key,
+    home_team_id bigserial,
+    away_team_id bigserial,
+    starts_at timestamp not null,
+    foreign key (home_team_id) references teams (id) on update cascade on delete restrict,
+    foreign key (away_team_id) references teams(id) on update cascade on delete restrict,
+    unique (home_team_id,away_team_id,starts_at)
+);
+
+create table if not exists aliases (
+    id bigserial primary key,
+    team_id bigserial,
+    alias varchar(64) not null unique,
+    foreign key (team_id) references teams (id) on update cascade on delete restrict
+);
+
+create table if not exists subscriptions (
+    id bigserial primary key,
+    url text unique,
+    match_id bigserial,
+    key text,
+    created_at timestamp not null,
+    notified_at timestamp,
+    foreign key (match_id) references matches (id) on update cascade on delete restrict
+);
+
+create table if not exists football_api_fixtures
+(
+    id bigserial primary key,
+    match_id bigserial,
+    data json,
+    foreign key (match_id) references matches (id) on update cascade on delete restrict
+);
+
+commit;
